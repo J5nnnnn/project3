dadc4444d35de9d270e552a9d18f24dddb97f84b
J5nnnnn <55269852+J5nnnnn@users.noreply.github.com>
2022-12-06 20:19:38 -0800

    minors updated

--- server/db/post.model.js
+++ server/db/post.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const PostSchema = require('./post.schema').PostSchema
 
-const PostModel = mongoose.model("post", PostSchema);
+const PostModel = mongoose.model("Post", PostSchema);
 
 function createPost(post){
     return PostModel.create(post);
